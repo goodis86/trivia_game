801030a3e7ee7096d467c7100394382f3bc474a6
Sandro Guraspasvili <gura.san@gmail.com>
2017-12-11 14:43:13 -0500

    figuring out the logic of the game, created objects for questions and the main game class

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,287 @@
-console.log('JS Engaged!')
+ class TriviaGame {
+     constructor() {
+         this.randomNumber = Math.floor(Math.random() * 20)
+         
+     }
+ }
+
+var question1 = {
+    question: '',
+    answ1: '',
+    answ2: '',
+    answ3: '',
+    answ4: '',
+    corAnsw: '',
+    check: function (click) {
+      if (click === this.corAnsw) {
+        console.log(`You've got it!`)
+      } else { console.log('Nope!') }
+    }
+  }
+  
+  var question2 = {
+    question: '',
+    answ1: '',
+    answ2: '',
+    answ3: '',
+    answ4: '',
+    corAnsw: '',
+    check: function (click) {
+      if (click === this.corAnsw) {
+        console.log(`You've got it!`)
+      } else { console.log('Nope!') }
+    }
+  }
+  
+  var question3 = {
+    question: '',
+    answ1: '',
+    answ2: '',
+    answ3: '',
+    answ4: '',
+    corAnsw: '',
+    check: function (click) {
+      if (click === this.corAnsw) {
+        console.log(`You've got it!`)
+      } else { console.log('Nope!') }
+    }
+  }
+  
+  var question4 = {
+    question: '',
+    answ1: '',
+    answ2: '',
+    answ3: '',
+    answ4: '',
+    corAnsw: '',
+    check: function (click) {
+      if (click === this.corAnsw) {
+        console.log(`You've got it!`)
+      } else { console.log('Nope!') }
+    }
+  }
+  
+  var question5 = {
+    question: '',
+    answ1: '',
+    answ2: '',
+    answ3: '',
+    answ4: '',
+    corAnsw: '',
+    check: function (click) {
+      if (click === this.corAnsw) {
+        console.log(`You've got it!`)
+      } else { console.log('Nope!') }
+    }
+  }
+  
+  var question6 = {
+    question: '',
+    answ1: '',
+    answ2: '',
+    answ3: '',
+    answ4: '',
+    corAnsw: '',
+    check: function (click) {
+      if (click === this.corAnsw) {
+        console.log(`You've got it!`)
+      } else { console.log('Nope!') }
+    }
+  }
+  
+  var question7 = {
+    question: '',
+    answ1: '',
+    answ2: '',
+    answ3: '',
+    answ4: '',
+    corAnsw: '',
+    check: function (click) {
+      if (click === this.corAnsw) {
+        console.log(`You've got it!`)
+      } else { console.log('Nope!') }
+    }
+  }
+  
+  var question8= {
+    question: '',
+    answ1: '',
+    answ2: '',
+    answ3: '',
+    answ4: '',
+    corAnsw: '',
+    check: function (click) {
+      if (click === this.corAnsw) {
+        console.log(`You've got it!`)
+      } else { console.log('Nope!') }
+    }
+  }
+  
+  var question9 = {
+    question: '',
+    answ1: '',
+    answ2: '',
+    answ3: '',
+    answ4: '',
+    corAnsw: '',
+    check: function (click) {
+      if (click === this.corAnsw) {
+        console.log(`You've got it!`)
+      } else { console.log('Nope!') }
+    }
+  }
+  
+  var question10 = {
+    question: '',
+    answ1: '',
+    answ2: '',
+    answ3: '',
+    answ4: '',
+    corAnsw: '',
+    check: function (click) {
+      if (click === this.corAnsw) {
+        console.log(`You've got it!`)
+      } else { console.log('Nope!') }
+    }
+  }
+  
+  var question11 = {
+    question: '',
+    answ1: '',
+    answ2: '',
+    answ3: '',
+    answ4: '',
+    corAnsw: '',
+    check: function (click) {
+      if (click === this.corAnsw) {
+        console.log(`You've got it!`)
+      } else { console.log('Nope!') }
+    }
+  }
+  
+  var question12 = {
+    question: '',
+    answ1: '',
+    answ2: '',
+    answ3: '',
+    answ4: '',
+    corAnsw: '',
+    check: function (click) {
+      if (click === this.corAnsw) {
+        console.log(`You've got it!`)
+      } else { console.log('Nope!') }
+    }
+  }
+  
+  var question13 = {
+    question: '',
+    answ1: '',
+    answ2: '',
+    answ3: '',
+    answ4: '',
+    corAnsw: '',
+    check: function (click) {
+      if (click === this.corAnsw) {
+        console.log(`You've got it!`)
+      } else { console.log('Nope!') }
+    }
+  }
+  
+  var question14 = {
+    question: '',
+    answ1: '',
+    answ2: '',
+    answ3: '',
+    answ4: '',
+    corAnsw: '',
+    check: function (click) {
+      if (click === this.corAnsw) {
+        console.log(`You've got it!`)
+      } else { console.log('Nope!') }
+    }
+  }
+  
+  var question15 = {
+    question: '',
+    answ1: '',
+    answ2: '',
+    answ3: '',
+    answ4: '',
+    corAnsw: '',
+    check: function (click) {
+      if (click === this.corAnsw) {
+        console.log(`You've got it!`)
+      } else { console.log('Nope!') }
+    }
+  }
+  
+  var question16 = {
+    question: '',
+    answ1: '',
+    answ2: '',
+    answ3: '',
+    answ4: '',
+    corAnsw: '',
+    check: function (click) {
+      if (click === this.corAnsw) {
+        console.log(`You've got it!`)
+      } else { console.log('Nope!') }
+    }
+  }
+  
+  var question17 = {
+    question: '',
+    answ1: '',
+    answ2: '',
+    answ3: '',
+    answ4: '',
+    corAnsw: '',
+    check: function (click) {
+      if (click === this.corAnsw) {
+        console.log(`You've got it!`)
+      } else { console.log('Nope!') }
+    }
+  }
+  
+  var question18 = {
+    question: '',
+    answ1: '',
+    answ2: '',
+    answ3: '',
+    answ4: '',
+    corAnsw: '',
+    check: function (click) {
+      if (click === this.corAnsw) {
+        console.log(`You've got it!`)
+      } else { console.log('Nope!') }
+    }
+  }
+  
+  var question19 = {
+    question: '',
+    answ1: '',
+    answ2: '',
+    answ3: '',
+    answ4: '',
+    corAnsw: '',
+    check: function (click) {
+      if (click === this.corAnsw) {
+        console.log(`You've got it!`)
+      } else { console.log('Nope!') }
+    }
+  }
+  
+  var question20 = {
+    question: '',
+    answ1: '',
+    answ2: '',
+    answ3: '',
+    answ4: '',
+    corAnsw: '',
+    check: function (click) {
+      if (click === this.corAnsw) {
+        console.log(`You've got it!`)
+      } else { console.log('Nope!') }
+    }
+  }
+  

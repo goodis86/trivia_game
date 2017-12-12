332c4c3ecc4ad6810cd42c8ead4735be75c0ca39
Sandro Guraspasvili <gura.san@gmail.com>
2017-12-12 11:33:23 -0500

    working at DWI - logic is set and almost finished

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,52 @@
-//  class TriviaGame {
-//      constructor() {
-//          this.randomNumber = Math.floor(Math.random() * 20)
-//      }
+let qField = $('q_field').html()
+let answ1 = $('answ1').html()
+let answ2 = $('answ2').html()
+let answ3 = $('answ3').html()
+let answ4 = $('answ4').html()
+let body = $('body')
 
-//      questionPrinter (randomNumber) {
-        
-//      }
-//  }
+class TriviaGame {
+  constructor () {
+    this.number = Math.floor(Math.random() * 4)
 
-var guestions = {
-  question1: {
-    question: '',
-    answ1: '',
-    answ2: '',
-    answ3: '',
-    answ4: '',
-    corAnsw: '',
+    qField = questions.number.question
+    answ1 = questions.number.answ1
+    answ2 = questions.number.answ2
+    answ3 = questions.number.answ3
+    answ4 = questions.number.answ4
+    // counter starts at 1
+    
+    body.addEventListener('click', checkAnswer() {
+        if (click === this.corAnsw) {
+            console.log(`You've got it!`)
+        } else { console.log('Nope!') }
+              
+    })
+  }
+//   checkAnswer (click) {
+//     if (click === this.corAnsw) {
+//       console.log(`You've got it!`)
+//     } else { console.log('Nope!') }
+//   }
+ }
+
+let questions = {
+  1: {
+    question: `What is Sandro's name?`,
+    answ1: 'Sandro',
+    answ2: 'David',
+    answ3: 'Zack',
+    answ4: 'Pete',
+    corAnsw: 'Sandro'
+  },
+
+  2: {
+    question: `What is Zack's name?`,
+    answ1: 'Sandro',
+    answ2: 'David',
+    answ3: 'Zack',
+    answ4: 'Pete',
+    corAnsw: 'Zack',
     check: function (click) {
       if (click === this.corAnsw) {
         console.log(`You've got it!`)
@@ -23,13 +54,13 @@ var guestions = {
     }
   },
 
-  question2: {
-    question: '',
-    answ1: '',
-    answ2: '',
-    answ3: '',
-    answ4: '',
-    corAnsw: '',
+  3: {
+    question: `What is David's name?`,
+    answ1: 'Sandro',
+    answ2: 'David',
+    answ3: 'Zack',
+    answ4: 'Pete',
+    corAnsw: 'David',
     check: function (click) {
       if (click === this.corAnsw) {
         console.log(`You've got it!`)
@@ -37,13 +68,13 @@ var guestions = {
     }
   },
 
-  question3: {
-    question: '',
-    answ1: '',
-    answ2: '',
-    answ3: '',
-    answ4: '',
-    corAnsw: '',
+  4: {
+    question: `What is Pete's name?`,
+    answ1: 'Sandro',
+    answ2: 'David',
+    answ3: 'Zack',
+    answ4: 'Pete',
+    corAnsw: 'Pete',
     check: function (click) {
       if (click === this.corAnsw) {
         console.log(`You've got it!`)

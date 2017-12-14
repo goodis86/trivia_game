bb776b9f2aba819f2aa8ca34bb8b4123a03f0b13
Gura-San <gura.san@gmail.com>
2017-12-13 21:20:59 -0500

    adding the flip effect to question field

--- scripts/script.js
+++ scripts/script.js
@@ -23,189 +23,188 @@ var lvlCounter = 1
   to tags conveniently established in the head.
 
   2. Also constructor launches the eventListener for clicks which
-  in it's term lauches the checkAnswerw()
+  in it's term launches the checkAnswers()
 
-  3. checkAnswerw() provides a check and iterates the counters.
+  3. checkAnswers() provides a check and iterates the counters.
   After that it launches the nextLevel() which iterates the main
-  caunter. It's used for the ID pickup from the DB. And relaunching
+  counter. It's used for the ID pickup from the DB. And relaunching
   the this.newLvl() with iterated ID.
 */
 
 class TriviaGame {
-  constructor () {
-    
-    // The main construction proccess of the game is fired up below
-    this.newLvl(lvlCounter)
-    
-    // The main event listener that registers the click
-      section.click(this.checkAnswer.bind(this));
-    
-  }
+    constructor() {
 
-  // Question population is done below
-  newLvl(lvlCounter) {
-    console.log(`lvlCounter in newLvl=${lvlCounter}`)
-    qField.html(questions[lvlCounter].question) 
-    answ1.html(questions[lvlCounter].answ1)
-    answ2.html(questions[lvlCounter].answ2)
-    answ3.html(questions[lvlCounter].answ3)
-    answ4.html(questions[lvlCounter].answ4)
-  }
+        // The main construction proccess of the game is fired up below
+        this.newLvl(lvlCounter)
 
-  /* Next level is fired up below, also there is a check
-   so you wouldn't play too long :) */
-  nextLevel () {
-    if (lvlCounter === 3) {this.terminator(); return}
-    else {
-      ++lvlCounter
-      questionNum.html(`${lvlCounter}/10`)
-      console.log(`lvlCounter=${lvlCounter}`)
-      this.newLvl(lvlCounter)
+        // The main event listener that registers the click
+        section.click(this.checkAnswer.bind(this));
+
     }
-  }
 
-  //
-  checkAnswer(event) {
-    // =================== checks and logs ==================
-    console.log(`event target= ${$(event.target).html()}`)
-    console.log(`corAnsw= ${questions[lvlCounter].corAnsw}`)
-    // ======================================================
-
-    if ($(event.target).html() === questions[lvlCounter].corAnsw) {
-      points.html(`${++rightAnswers} pnts`)
-      console.log(`You're right!!! C/A ${questions[lvlCounter].corAnsw}`)
-
-      this.nextLevel()
-    } else {
-      console.log(`You're wrong!!!`)
-      this.nextLevel()
+    // Question population is done below
+    newLvl(lvlCounter) {
+        console.log(`lvlCounter in newLvl=${lvlCounter}`)
+        qField.html(questions[lvlCounter].question)
+        answ1.html(questions[lvlCounter].answ1)
+        answ2.html(questions[lvlCounter].answ2)
+        answ3.html(questions[lvlCounter].answ3)
+        answ4.html(questions[lvlCounter].answ4)
     }
-  }
 
-  terminator(){
-    console.log(`Terminator online! Terminating Sarah Connor!`)
-    $('.section2').off();
-  }
- }
+    /* Next level is fired up below, also there is a check
+     so you wouldn't play too long :) */
+    nextLevel() {
+        if (lvlCounter === 3) {
+            this.terminator();
+            return
+        }
+        else {
+            ++lvlCounter
+            questionNum.html(`${lvlCounter}/10`)
+            console.log(`lvlCounter=${lvlCounter}`)
+            this.newLvl(lvlCounter)
+        }
+    }
 
-let questions = {
-  1: {
-    question: `What is Sandro's name?`,
-    answ1: 'Sandro',
-    answ2: 'David',
-    answ3: 'Zack',
-    answ4: 'Pete',
-    corAnsw: 'Sandro'
-  },
+    //
+    checkAnswer(event) {
+        // =================== checks and logs ==================
+        console.log(`event target= ${$(event.target).html()}`)
+        console.log(`corAnsw= ${questions[lvlCounter].corAnsw}`)
+        // ======================================================
 
-  2: {
-    question: `What is Zack's name?`,
-    answ1: 'Sandro',
-    answ2: 'David',
-    answ3: 'Zack',
-    answ4: 'Pete',
-    corAnsw: 'Zack'
-  },
+        if ($(event.target).html() === questions[lvlCounter].corAnsw) {
+            points.html(`${++rightAnswers} pnts`)
+            $("#card").flip();
+            console.log(`You're right!!! C/A ${questions[lvlCounter].corAnsw}`)
 
-  3: {
-    question: `What is David's name?`,
-    answ1: 'Sandro',
-    answ2: 'David',
-    answ3: 'Zack',
-    answ4: 'Pete',
-    corAnsw: 'David'
-  },
+            this.nextLevel()
+        } else {
+            console.log(`You're wrong!!!`)
+            this.nextLevel()
+        }
+    }
 
-  4: {
-    question: `What is Pete's name?`,
-    answ1: 'Sandro',
-    answ2: 'David',
-    answ3: 'Zack',
-    answ4: 'Pete',
-    corAnsw: 'Pete'
-  },
+    animate() {
 
-  5: {
-    question: `What is Sandro's name?`,
-    answ1: 'Sandro',
-    answ2: 'David',
-    answ3: 'Zack',
-    answ4: 'Pete',
-    corAnsw: 'Sandro'
-  },
+    }
 
-  6: {
-    question: `What is Zack's name?`,
-    answ1: 'Sandro',
-    answ2: 'David',
-    answ3: 'Zack',
-    answ4: 'Pete',
-    corAnsw: 'Zack'
-  },
-
-  7: {
-    question: `What is David's name?`,
-    answ1: 'Sandro',
-    answ2: 'David',
-    answ3: 'Zack',
-    answ4: 'Pete',
-    corAnsw: 'David'
-  },
-
-  8: {
-    question: `What is Pete's name?`,
-    answ1: 'Sandro',
-    answ2: 'David',
-    answ3: 'Zack',
-    answ4: 'Pete',
-    corAnsw: 'Pete'
-  },
-
-  9: {
-    question: `What is Sandro's name?`,
-    answ1: 'Sandro',
-    answ2: 'David',
-    answ3: 'Zack',
-    answ4: 'Pete',
-    corAnsw: 'Sandro'
-  },
-
-  10: {
-    question: `What is Zack's name?`,
-    answ1: 'Sandro',
-    answ2: 'David',
-    answ3: 'Zack',
-    answ4: 'Pete',
-    corAnsw: 'Zack'
-  },
-
-  11: {
-    question: `What is David's name?`,
-    answ1: 'Sandro',
-    answ2: 'David',
-    answ3: 'Zack',
-    answ4: 'Pete',
-    corAnsw: 'David'
-  },
-
-  12: {
-    question: `What is Pete's name?`,
-    answ1: 'Sandro',
-    answ2: 'David',
-    answ3: 'Zack',
-    answ4: 'Pete',
-    corAnsw: 'Pete'
-  }
+    terminator() {
+        console.log(`Terminator online! Terminating Sarah Connor!`)
+        $('.section2').off();
+    }
 }
 
-//JSon engaged. Formating ans storing data in JSon file
-// var dataStore = JSON.stringify(questions)
-// storage.setItem('someData', dataToStore);
-// var localData = JSON.parse(localStorage.getItem('someData'));
+let questions = {
+    1: {
+        question: `What is Sandro's name?`,
+        answ1: 'Sandro',
+        answ2: 'David',
+        answ3: 'Zack',
+        answ4: 'Pete',
+        corAnsw: 'Sandro'
+    },
 
-// $.each(localData, function(key, value){
-//   console.log(key + ' = ' + value);
-// });
+    2: {
+        question: `What is Zack's name?`,
+        answ1: 'Sandro',
+        answ2: 'David',
+        answ3: 'Zack',
+        answ4: 'Pete',
+        corAnsw: 'Zack'
+    },
 
-// I won't tell you what this string below does, sorry.
+    3: {
+        question: `What is David's name?`,
+        answ1: 'Sandro',
+        answ2: 'David',
+        answ3: 'Zack',
+        answ4: 'Pete',
+        corAnsw: 'David'
+    },
+
+    4: {
+        question: `What is Pete's name?`,
+        answ1: 'Sandro',
+        answ2: 'David',
+        answ3: 'Zack',
+        answ4: 'Pete',
+        corAnsw: 'Pete'
+    },
+
+    5: {
+        question: `What is Sandro's name?`,
+        answ1: 'Sandro',
+        answ2: 'David',
+        answ3: 'Zack',
+        answ4: 'Pete',
+        corAnsw: 'Sandro'
+    },
+
+    6: {
+        question: `What is Zack's name?`,
+        answ1: 'Sandro',
+        answ2: 'David',
+        answ3: 'Zack',
+        answ4: 'Pete',
+        corAnsw: 'Zack'
+    },
+
+    7: {
+        question: `What is David's name?`,
+        answ1: 'Sandro',
+        answ2: 'David',
+        answ3: 'Zack',
+        answ4: 'Pete',
+        corAnsw: 'David'
+    },
+
+    8: {
+        question: `What is Pete's name?`,
+        answ1: 'Sandro',
+        answ2: 'David',
+        answ3: 'Zack',
+        answ4: 'Pete',
+        corAnsw: 'Pete'
+    },
+
+    9: {
+        question: `What is Sandro's name?`,
+        answ1: 'Sandro',
+        answ2: 'David',
+        answ3: 'Zack',
+        answ4: 'Pete',
+        corAnsw: 'Sandro'
+    },
+
+    10: {
+        question: `What is Zack's name?`,
+        answ1: 'Sandro',
+        answ2: 'David',
+        answ3: 'Zack',
+        answ4: 'Pete',
+        corAnsw: 'Zack'
+    },
+
+    11: {
+        question: `What is David's name?`,
+        answ1: 'Sandro',
+        answ2: 'David',
+        answ3: 'Zack',
+        answ4: 'Pete',
+        corAnsw: 'David'
+    },
+
+    12: {
+        question: `What is Pete's name?`,
+        answ1: 'Sandro',
+        answ2: 'David',
+        answ3: 'Zack',
+        answ4: 'Pete',
+        corAnsw: 'Pete'
+    }
+}
+
+// Creating the instance of the game below
 var Game = new TriviaGame()
